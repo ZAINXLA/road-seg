2e231a05b27c15c4f6d4233063a89f0d5adbf156
ZAINXLA <78021512+ZAINXLA@users.noreply.github.com>
2023-12-12 23:08:07 +0000

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ EPOCHS = 20 # Number of the epochs
 SAVE_EVERY = 5 # after how many epochs to save a checkpoint
 LOG_EVERY = 1 #  log training and validation metrics every `LOG_EVERY` epochs
 BATCH_SIZE = 16
-DEVICE = 'cuda'  
+DEVICE = 'cpu'  
 LR = 0.0001
 ROOT_PATH = 'CARL_DATASET'
 
