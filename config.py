5b21231c26f38d2f6f6f37a1e7c8a54ea1ef3643
ZAINXLA <78021512+ZAINXLA@users.noreply.github.com>
2023-12-12 21:11:02 +0000

    Add files via upload

---
+++ config.py
@@ -0,0 +1,12 @@
+EPOCHS = 20 # Number of the epochs
+SAVE_EVERY = 5 # after how many epochs to save a checkpoint
+LOG_EVERY = 1 #  log training and validation metrics every `LOG_EVERY` epochs
+BATCH_SIZE = 16
+DEVICE = 'cuda'  
+LR = 0.0001
+ROOT_PATH = 'CARL_DATASET'
+
+# the classes that we want to train
+CLASSES_TO_TRAIN = ['road', 'background']
+# DEBUG for visualizations
+DEBUG = True
